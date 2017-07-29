34b3c18012db09c518774edd66fabdea39a1225e
55_maximus_55 <lukinosh55@gmail.com>
2017-07-29 08:07:45 +0600

    libgdx project

---
+++ android/src/ru/codemonkey/studio/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package ru.codemonkey.studio;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import ru.codemonkey.studio.Power;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Power(), config);
+	}
+}
